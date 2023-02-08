04cdabe871a02fce437311e54ffc6ec8350c8f98
sahishnusuresh <sahishnusuresh@gmail.com>
2023-02-08 12:05:58 +0530

    mailosaur mail api usage for oslash login

---
+++ tests/example.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+import MailosaurClient from 'mailosaur';
+import MailoSaurClient from "mailosaur"
+import fetch from "node-fetch"
+import dotenv from 'dotenv'
+const prefix="HELLO5"
+dotenv.config()
+const server_id=process.env.server_id
+const api_key=process.env.api_key
+const company_name="oslash"
+
+test('has title', async ({ page }) => {
+  await page.goto('https://app.oslash.com/login/');
+  const email=`${prefix}-prod-bot@${server_id}.mailosaur.net`
+  await page.locator('input#email').fill(email)
+  const testdate=new Date()
+  await page.getByRole('button',{name:"Send"}).click()
+  const mailosaur=new MailosaurClient(api_key)
+  const testemail=await mailosaur.messages.get(server_id,{
+    sentTo:email,},{receivedAfter:testdate})
+  console.log(testemail.html?.links)
+const links=testemail.html?.links as Array<HTMLLinkElement>
+await page.goto(links[1].href)
+await page.locator('input#workspaceName').fill(prefix)
+await page.locator('input#companyName').fill(company_name)
+const delay=async (time)=>new Promise(resolve=> setTimeout(resolve,time))
+await delay(4000)
+await page.locator('button',{hasText:"Start 15-day Free Trial"}).click()
+
+await page.pause()
+});
+
+
